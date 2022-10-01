707730e108b9df6a4bdd294223fc5d0941dd2e37
B <B@aravinths-mbp.home>
2022-10-01 21:01:45 +0100

    Articles

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,7 +17,7 @@ export default function Cards() {
                             <p>
                                 If all the tokens are staked inflation = apy. If the tokens staked is lower the returns will he higher and vice versa
                             </p>
-                            <a href="#" class="btn">Read more</a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@staking7pc/how-total-staked-affects-apy-in-axelar-1d43602ef891" class="btn">Read more</a>
                         </div>
                     </div>
                     <div class="card">
@@ -26,9 +26,9 @@ export default function Cards() {
                         </div>
                         <div class="card-body">
                             <p>
-                                Each external chain gets 1% inflation rewards currently. So validator with maximum chains supported will get maximum returns
+                                Each external chain gets 1% inflation rewards currently. So validator with maximum chains will get max returns
                             </p>
-                            <a href="#" class="btn">Read more</a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@staking7pc/how-external-chains-affect-your-rewards-in-axelar-b683f96b492e" class="btn">Read more</a>
                         </div>
                     </div>
                     <div class="card">
@@ -39,7 +39,7 @@ export default function Cards() {
                             <p>
                             Minimum commission for a validator is 5%. To get higher returns choose the validator with lowest commission.
                             </p>
-                            <a href="#" class="btn">Read more</a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@staking7pc/how-commission-affect-rewards-in-staking-axelar-96bdc81ff999" class="btn">Read more</a>
                         </div>
                     </div>
                     <div class="card">
@@ -50,7 +50,7 @@ export default function Cards() {
                             <p>
                                 Slashing can be of 2 types, where you lose your capital and where you dont loose the capital but lose rewards
                             </p>
-                            <a href="#" class="btn">Read more</a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@staking7pc/slashing-in-axelar-fd084eed6852" class="btn">Read more</a>
                         </div>
                     </div>
                 </div>
