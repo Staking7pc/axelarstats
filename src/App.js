a274cb411cd344c3f8e058fd7a9562421064e213
B <B@aravinths-mbp.home>
2022-10-02 09:23:37 +0100

    copyright

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ValidatorInfo from "./components/ValidatorInfo";
 import Header from "./components/Header";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Cards />
       <ValidatorInfo />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+export default function Header() {
+  return (
+    <div>
+      <p> We have done our best to provide information as accurate as possible. There could be some discrepancies. If you encouter an issue please report to us</p>
+      <p>Brightstake Ⓒ 2022</p>
+    </div>
+  )
+}
